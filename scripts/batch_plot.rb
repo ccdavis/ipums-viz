c969e9e5d6b197b8859d894e65feb104e8487178
Colin Davis <colin.c.davis@gmail.com>
2019-12-27 18:06:06 -0600

    sum up total records in each project

--- scripts/batch_plot.rb
+++ scripts/batch_plot.rb
@@ -1,4 +1,26 @@
 #!/usr/bin/env ruby
+
+
+require 'csv'
+
+
+data_dir = File.join(File.expand_path(File.dirname(__FILE__)),"..","data")
+	
+data_sizes = Dir.entries(data_dir).select{|s| s.include?("data_sizes_live")}
+
+total_records = 0
+
+data_sizes.each do |size_file|
+	size_path = File.join(data_dir, size_file)
+
+	data = CSV.read(size_path, headers: true)
+	project_records =data.map{|row| row["records"].to_i}.reduce(:+)
+	puts "#{size_file} :   #{project_records}"
+	total_records += project_records
+end
+
+puts "Total: #{total_records}"
+
 list = `ls ../data/data_sizes_*.csv`.split("\n").map{|l| l.strip.gsub(".csv","")}
 
 list.each do | fname |
